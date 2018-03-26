2ff7406855f011b755d686030d33db3e18f9c2b6
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-03-26 22:56:05 +0300

    modified task4_1.sh

--- task4_1.sh
+++ task4_1.sh
@@ -3,12 +3,12 @@ exec 1>task4_1.out
 echo "--- Hardware ---"
 echo "CPU:"; lscpu | grep "Model name" | cut -d : -f 2
 echo "RAM: "; sudo dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2
-echo "Motherboard: "
-echo "System Serial Number: "
+echo "Motherboard: "sudo dmidecode -t 2 | grep "Product Name" | cut -d : -f 2
+echo "System Serial Number: "; sudo dmidecode -s system-serial-number
 echo "--- System ---"
-echo "OS Distribution: "
+echo "OS Distribution: " lsb_release -d | cut -d : -f 2
 echo "Kernel version:" $(uname -r)
-echo "Installation date: "
+echo "Installation date: " 
 echo "Hostname:" $(hostname)
 echo "Uptime: "
 echo "Processes running: "
