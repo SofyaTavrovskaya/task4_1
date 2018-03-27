5812e7740e71d235d026d1bb1db3691c14f0b141
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-03-27 21:36:35 +0300

    next commit

--- task4_1.sh
+++ task4_1.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 exec 1>task4_1.out
 echo "--- Hardware ---"
-echo "CPU:"; lscpu | grep "Model name" | cut -d : -f 2
-echo "RAM: "; sudo dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2
-echo "Motherboard: "sudo dmidecode -t 2 | grep "Product Name" | cut -d : -f 2
-echo "System Serial Number: "; sudo dmidecode -s system-serial-number
+echo "CPU:" $(lscpu | grep "Model name" | cut -d : -f 2)
+echo "RAM: " $(dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2)
+echo "Motherboard: " $(dmidecode -t 2 | grep "Product Name" | cut -d : -f 2)
+echo "System Serial Number: " $(dmidecode -s system-serial-number)
 echo "--- System ---"
-echo "OS Distribution: " lsb_release -d | cut -d : -f 2
+echo "OS Distribution: " $(lsb_release -d | cut -d : -f 2)
 echo "Kernel version:" $(uname -r)
-echo "Installation date: " 
+echo "Installation date: " $(ls -alct /|tail -1|awk '{print $6, $7, $8}')
 echo "Hostname:" $(hostname)
-echo "Uptime: "
-echo "Processes running: "
-echo "User logged in: "
+echo "Uptime: " $(uptime -p| awk -F'( |,)' '{print $2, $3}')
+echo "Processes running: " $(ps aux | wc -l)
+echo "User logged in: " $(who | sort -k1,1 -u | wc -l)
 echo "--- Network ---"
-for interface in $(ifconfig -l); do
-            echo $interface
-        done
+echo $(ip -o -a addr show|tr -s ' '| cut -d ' ' -f 2,4)
+
