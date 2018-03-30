afacd58669706296124a83d1147707054b6e40ec
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-03-30 23:45:39 +0300

    maybe last commit

--- task4_1.sh
+++ task4_1.sh
@@ -2,17 +2,31 @@
 exec 1>task4_1.out
 echo "--- Hardware ---"
 echo "CPU:" $(lscpu | grep "Model name" | cut -d : -f 2)
-echo "RAM: " $(dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2)
-echo "Motherboard: " $(dmidecode -t 2 | grep "Product Name" | cut -d : -f 2)
-echo "System Serial Number: " $(dmidecode -s system-serial-number)
+echo "RAM:" $(dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2)
+MOTHERBOARD=$(dmidecode -s baseboard-manufacturer) $(dmidecode -s baseboard-product-name)
+[[ -z $MOTHERBOARD ]]&&MOTHERBOARD=Unknown
+echo "Motherboard:" $MOTHERBOARD
+SERIAL=$(dmidecode -s system-serial-number)
+[[ -z $SERIAL ]] && SERIAL=Unknown
+echo "System Serial Number:" $SERIAL
 echo "--- System ---"
-echo "OS Distribution: " $(lsb_release -d | cut -d : -f 2)
+echo "OS Distribution:" $(lsb_release -d | cut -d : -f 2)
 echo "Kernel version:" $(uname -r)
-echo "Installation date: " $(ls -alct /|tail -1|awk '{print $6, $7, $8}')
+echo "Installation date:" $(ls -alct /|tail -1|awk '{print $6, $7, $8}')
 echo "Hostname:" $(hostname)
-echo "Uptime: " $(uptime -p| awk -F'( |,)' '{print $2, $3}')
-echo "Processes running: " $(ps aux | wc -l)
-echo "User logged in: " $(who | sort -k1,1 -u | wc -l)
+echo "Uptime:" $(uptime | awk -F'( |,|:)+' '{print $6,$7",",$8,"hours,",$9,"minutes"}')
+echo "Processes running:" $(ps aux | wc -l)
+echo "User logged in:" $(who | sort -k1,1 -u | wc -l)
 echo "--- Network ---"
-echo $(ip -o -a addr show|tr -s ' '| cut -d ' ' -f 2,4)
+for interface in $(ls /sys/class/net); do
+     # echo $interface
+     is_up=$(ip -o addr show $interface | wc -l)
+      # echo $is_up
+     if ((is_up > 0)); then
+         echo -e "$interface: \c"
+     ip -o -4 -a addr show $interface|tr -s ' '| cut -d ' ' -f 4| paste -d',' -s|sed 's/,/, /'
+         else
+             echo "$interface: -"
+             fi
+         done
 
