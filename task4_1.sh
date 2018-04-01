3fe672010bc1707c8b319962c8f0c898f0fb64ae
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-04-01 12:39:32 +0300

    correct output of motherboard and uptime

--- task4_1.sh
+++ task4_1.sh
@@ -3,9 +3,16 @@ exec 1>task4_1.out
 echo "--- Hardware ---"
 echo "CPU:" $(lscpu | grep "Model name" | cut -d : -f 2)
 echo "RAM:" $(dmidecode -t memory | grep -i size | head -n 1 | cut -d : -f 2)
-MOTHERBOARD=$(dmidecode -s baseboard-manufacturer) $(dmidecode -s baseboard-product-name)
+MANUFACTURER=$(dmidecode -s baseboard-manufacturer)
+MOTHERBOARD=$(dmidecode -s baseboard-product-name)
 [[ -z $MOTHERBOARD ]]&&MOTHERBOARD=Unknown
-echo "Motherboard:" $MOTHERBOARD
+[[ -z $MANUFACTURER ]]&&MANUFACTURER=Unknown
+#if we have Unknown twice in answer, return only one Unknown
+if [[ $MOTHERBOARD == "Unknown" && $MANUFACTURER=="Unknown" ]] ; then
+echo "Motherboard: Unknown"
+else 
+echo "Motherboard:" $MANUFACTURER $MOTHERBOARD 
+fi
 SERIAL=$(dmidecode -s system-serial-number)
 [[ -z $SERIAL ]] && SERIAL=Unknown
 echo "System Serial Number:" $SERIAL
@@ -13,8 +20,8 @@ echo "--- System ---"
 echo "OS Distribution:" $(lsb_release -d | cut -d : -f 2)
 echo "Kernel version:" $(uname -r)
 echo "Installation date:" $(ls -alct /|tail -1|awk '{print $6, $7, $8}')
-echo "Hostname:" $(hostname)
-echo "Uptime:" $(uptime | awk -F'( |,|:)+' '{print $6,$7",",$8,"hours,",$9,"minutes"}')
+echo "Hostname:" $(hostname -f)
+echo "Uptime:" $(uptime -p | cut -d " " -f2-)
 echo "Processes running:" $(ps aux | wc -l)
 echo "User logged in:" $(who | sort -k1,1 -u | wc -l)
 echo "--- Network ---"
